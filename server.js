de7fa477bcdc7ac880a743d5d667636687c1c950
Jay Bittner <jtbitt@gmail.com>
2017-03-19 19:57:15 -0700

    Set up basic file strstructure of app

---
+++ server.js
@@ -0,0 +1,17 @@
+// get packages
+var express = require('express');
+var app     = express();
+var path    = require('path');
+var port    = process.env.PORT || 8080;
+
+// set up front end
+app.use(express.static(__dirname + '/src'));
+
+// route to send index.html
+app.get('/', function(req, res)){
+	res.sendFile(path.join(__dirname + '/src/index.html'));
+});
+
+// start server
+app.listen(port);
+console.log('Expres server listening on ' + port);
