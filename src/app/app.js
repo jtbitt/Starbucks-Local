675fb9c0e06e4c22ea3ed55688dd40a7c2c25e5c
Jay Bittner <jtbitt@gmail.com>
2017-03-21 12:06:31 -0700

    Testing out google api

--- src/app/app.js
+++ src/app/app.js
@@ -1,26 +1,68 @@
+function grabGoogleInfo($http){
+    // return $http.get("https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=vSQEWdJoxXj1FBy2k5V5KQ&client_secret=3zl3pryBWLUyZNRzmHI3rfzslvp1bJVyeDntO4H0rUHH3IvDuizXKEHQ6CEUIduu").
+    //     then(function(data){
+    //         return data;
+    //     });
+
+    return $http.get('https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJN1t_tDeuEmsRUsoyG83frY4&key=AIzaSyDhG0sibDpt2fHINsCJwur8ZlJou5CDa6I').
+      then(function(data){
+        console.log(data);
+        return data;
+      }).error(function(err, status){
+        console.log(err)
+        console.log(status)
+      })   
+}
+
 function config($stateProvider, $locationProvider){
     $stateProvider
         .state('sbucks', {
             url: '/',
             templateUrl: 'app/app.html',
-            controller: 'AppCtrl as app'
+            controller: 'AppCtrl as app',
+            resolve: {
+              // cool: grabGoogleInfo
+            }  
         });
 
     $locationProvider.html5Mode(true);
 
 }
 
-function AppCtrl(){
+function AppCtrl($http){
 
-    var vm = this;
+    var app = this;
+
+    // $http({
+    //   method: 'GET', 
+    //   url:  'https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJN1t_tDeuEmsRUsoyG83frY4&key=AIzaSyDhG0sibDpt2fHINsCJwur8ZlJou5CDa6I'
+    //  }).then(function(data){
+    //     console.log(data);
+    //   })
+
+    console.log('init app')
+    app.fetch = function(){
+      console.log('oy')
+      fetch('https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJN1t_tDeuEmsRUsoyG83frY4&key=AIzaSyDhG0sibDpt2fHINsCJwur8ZlJou5CDa6I', {
+        method: 'GET'
+      }).then(function(resp){
+        console.log(resp.status)
+        return resp.json()
+      }).then(function(data){
+        console.log(data)
+      })
+    }
+
+      
+
+
+    // console.log(cool);
+
 
 }
 
 angular.module('sbucks', [
-    'ui.router',
-    'sbucks.santamonica',
-    'sbucks.newyork',
-    'sbucks.london'
+    'ui.router'
 ])
 
     .config(config)
