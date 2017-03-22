fcacbc4b476f87a90702606f8f5d0dd1feca1630
Jay Bittner <jay.bittner@sunrider.com>
2017-03-21 19:56:54 -0700

    Successfully pulling in Starbucks business info via Google api

--- src/app/app.js
+++ src/app/app.js
@@ -1,63 +1,37 @@
-function grabGoogleInfo($http){
-    // return $http.get("https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=vSQEWdJoxXj1FBy2k5V5KQ&client_secret=3zl3pryBWLUyZNRzmHI3rfzslvp1bJVyeDntO4H0rUHH3IvDuizXKEHQ6CEUIduu").
-    //     then(function(data){
-    //         return data;
-    //     });
-
-    return $http.get('https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJN1t_tDeuEmsRUsoyG83frY4&key=AIzaSyDhG0sibDpt2fHINsCJwur8ZlJou5CDa6I').
-      then(function(data){
-        console.log(data);
-        return data;
-      }).error(function(err, status){
-        console.log(err)
-        console.log(status)
-      })   
-}
-
 function config($stateProvider, $locationProvider){
     $stateProvider
         .state('sbucks', {
             url: '/',
             templateUrl: 'app/app.html',
-            controller: 'AppCtrl as app',
-            resolve: {
-              // cool: grabGoogleInfo
-            }  
+            controller: 'AppCtrl as app'
         });
 
     $locationProvider.html5Mode(true);
 
 }
 
-function AppCtrl($http){
+function AppCtrl($scope){
 
     var app = this;
 
-    // $http({
-    //   method: 'GET', 
-    //   url:  'https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJN1t_tDeuEmsRUsoyG83frY4&key=AIzaSyDhG0sibDpt2fHINsCJwur8ZlJou5CDa6I'
-    //  }).then(function(data){
-    //     console.log(data);
-    //   })
+    app.data;
 
-    console.log('init app')
-    app.fetch = function(){
-      console.log('oy')
-      fetch('https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJN1t_tDeuEmsRUsoyG83frY4&key=AIzaSyDhG0sibDpt2fHINsCJwur8ZlJou5CDa6I', {
-        method: 'GET'
-      }).then(function(resp){
-        console.log(resp.status)
-        return resp.json()
-      }).then(function(data){
-        console.log(data)
-      })
+    function getGoogleData(){
+        var service = new google.maps.places.PlacesService(map);
+        var place = { placeId: 'ChIJr4vVw8-kwoAR0iEipIyAZWw' };
+
+        service.getDetails(place, function(place, status) {
+                if (status === google.maps.places.PlacesServiceStatus.OK) {
+                    if(place) {
+                        console.log(place);
+                        app.data = place;
+                        $scope.$apply();
+                    }
+                }
+            }
+        );
     }
-
-      
-
-
-    // console.log(cool);
-
+    getGoogleData();
 
 }
 
